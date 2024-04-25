d16c5a83a0f54dbed4d6462fd5b8fe61a92500a8
DeVerMyst <antonyschutz@gmail.com>
2024-04-25 16:10:36 +0200

    first commit

---
+++ routes/routes.py
@@ -0,0 +1,32 @@
+# routes/routes.py 
+from flask import render_template
+from models.model_blog import ModelBlog
+
+
+def routes_setup(app):
+    @app.route('/')
+    def index():
+        return "Hello ! hello pas hello"
+
+    @app.route('/about')
+    def about():
+        return "Il fait beau et il n'y a pas trop de vent"
+
+    @app.route('/template')
+    def template():
+        return render_template("toto.html")
+
+    @app.route('/template/motif')
+    def motif():
+        return render_template("index.html")
+    
+    @app.route('/template/joli')
+    def joli():
+        return render_template("page2.html")    
+    
+    @app.route('/template/demain')
+    def demain():
+        
+        # créer ou vérifier que la base de données existe
+        
+        return render_template("page3.html")        
